ba5f5e221fb06b69bdbd3b1975f19e556bce2d63
Maksym <golovkomaksim852@gmail.com>
2026-02-19 10:40:29 +0200

    chore: setup path aliases, add variable fonts, and finalize FSD-lite structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,30 @@
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router";
-import Loader from "../shared/Loader";
+import Loader from "@/shared/Loader";
 
-const HomePage = lazy(() => import("../pages/homePage"));
+const HomePage = lazy(() => import("@/pages/homePage"));
 const SortingAlgorithmsPage = lazy(
-  () => import("../pages/sortingAlgorithmsPage"),
+  () => import("@/pages/sortingAlgorithmsPage"),
 );
-const OtherAlgorithmsPage = lazy(() => import("../pages/otherAlgorithmsPage"));
+const OtherAlgorithmsPage = lazy(() => import("@/pages/otherAlgorithmsPage"));
 const SearchingAlgorithmsPage = lazy(
-  () => import("../pages/searchingAlgorithmsPage"),
+  () => import("@/pages/searchingAlgorithmsPage"),
 );
-const NotFoundPage = lazy(() => import("../pages/notFoundPage"));
+const NotFoundPage = lazy(() => import("@/pages/notFoundPage"));
 
 function App() {
   return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sorting" element={<SortingAlgorithmsPage />} />
-        <Route path="/other" element={<OtherAlgorithmsPage />} />
-        <Route path="/search" element={<SearchingAlgorithmsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Suspense>
+    <>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sorting" element={<SortingAlgorithmsPage />} />
+          <Route path="/other" element={<OtherAlgorithmsPage />} />
+          <Route path="/search" element={<SearchingAlgorithmsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
